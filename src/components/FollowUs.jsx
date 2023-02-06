bbd3d64fbebd015a3ad3abc66ca4e688cebb58d0
Igor Budimir <igor.budimir.developer@gmail.com>
2023-02-06 01:44:40 +0100

    add dist for deploy

--- src/components/FollowUs.jsx
+++ src/components/FollowUs.jsx
@@ -5,16 +5,16 @@ import Yt from "../assets/yt.webp";
 
 const FollowUs = () => {
   return (
-    <div className=" bg-gray-200 flex flex-col pt-7">
-      <h2 className="text-gray-600 text-2xl text-center">Follow us on social media</h2>
-      <div className="flex flex-col items-center">
-        <div className="flex items-center">
-          <img src={Tw} className="max-w-[8em]" />
-          <img src={Fb} className="max-w-[8em]" />
+    <div className=" bg-gray-200 flex flex-col pt-7 xl:gap-7 xl:pt-9">
+      <h2 className="text-gray-600 text-2xl text-center xl:text-3xl">Follow us on social media</h2>
+      <div className="flex flex-col items-center xl:flex-row xl:justify-center xl:gap-2">
+        <div className="flex items-center xl:gap-2">
+          <img src={Tw} className="max-w-[8em] xl:max-w-[9em]" />
+          <img src={Fb} className="max-w-[8em] xl:max-w-[9em]" />
         </div>
-        <div className="flex items-center">
-          <img src={Ig} className="max-w-[8em]" />
-          <img src={Yt} className="max-w-[8em]" />
+        <div className="flex items-center xl:gap-2">
+          <img src={Ig} className="max-w-[8em] xl:max-w-[9em]" />
+          <img src={Yt} className="max-w-[8em] xl:max-w-[9em]" />
         </div>
       </div>
     </div>
--- src/components/Forspoken.jsx
+++ src/components/Forspoken.jsx
@@ -22,15 +22,15 @@ const Forspoken = () => {
         </div>
       </div>
       <div className="flex flex-col gap-7 items-center text-white text-center bg-[#26254f] md:flex-row md:px-7 xl:px-[15em] transition-all duration-75 hover:cursor-pointer pb-10">
-        <div className="max-w-[90%] flex flex-col items-center gap-5 hover:cursor-pointer transition-all duration-[0.3s] ease-out hover:translate-y-[-20px]">
+        <div className="max-w-[90%] flex flex-col items-center gap-5 hover:cursor-pointer transition-all duration-[0.3s] ease-out hover:scale-[1.05]">
           <img src={PreviewKeyart} className="rounded-[15px] shadow-lg shadow-black  " />
           <span className="font-semibold text-xl">23 great games to look forward to in 2023</span>
         </div>
-        <div className="max-w-[90%] flex flex-col items-center gap-5 hover:cursor-pointer transition-all duration-[0.3s] ease-out hover:translate-y-[-20px]">
+        <div className="max-w-[90%] flex flex-col items-center gap-5 hover:cursor-pointer transition-all duration-[0.3s] ease-out hover:scale-[1.05]">
           <img src={UltimateKeyart} className="rounded-[15px] shadow-lg shadow-black  " />
           <span className="font-semibold text-xl">The ultimate PlayStation review</span>
         </div>
-        <div className="max-w-[90%] flex flex-col items-center gap-5 transition-all duration-[0.3s] hover:cursor-pointer ease-out hover:translate-y-[-20px]">
+        <div className="max-w-[90%] flex flex-col items-center gap-5 transition-all duration-[0.3s] hover:cursor-pointer ease-out hover:scale-[1.05]">
           <img src={CozyKeyart} className="rounded-[15px] shadow-lg shadow-black " />
           <span className="font-semibold text-xl">Best cozy games on PS4 & PS5</span>
         </div>
